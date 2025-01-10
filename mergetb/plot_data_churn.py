a214224d94870eda944b3b0cac0d90b8fa7136e8
Derya <deryacog@gmail.com>
2025-01-10 20:11:19 +0100

    moer churn pltos

--- mergetb/plot_data_churn.py
+++ mergetb/plot_data_churn.py
@@ -3,7 +3,7 @@ import numpy as np
 import json
 import sys
 import os
-from plot_data import get_data, save_plot_directory, plot_latency_components, plot_reliability, plot_incoming_messages, plot_latency, plot_bandwidth, plot_latency_and_bandwidth, plot_reliability_latency
+from plot_data import get_data, save_plot_directory, plot_latency_components, plot_reliability, plot_incoming_messages, plot_latency, plot_bandwidth, plot_latency_and_bandwidth, plot_reliability_latency, convert_to_milliseconds
 from matplotlib.ticker import FuncFormatter
 
 def log_format(y, _):
@@ -37,10 +37,12 @@ if __name__ == "__main__":
             for retry in retry_levels
         }
 
-        plt.figure(figsize=(12, 8))
+        plt.figure(figsize=(20, 6))
 
         bar_width = 0.2
-        x_indexes = range(len(points_in_time_labels))
+        group_spacing = 0.1
+        x_indexes = np.arange(len(points_in_time_labels)) * (len(retry_levels) * bar_width + group_spacing)
+
         offset = 0
 
         plt.yscale('log')
@@ -66,6 +68,100 @@ if __name__ == "__main__":
         plt.savefig(os.path.join(directory, f"{variable}_reliability_churn_bar_chart.png"))
         plt.clf()
         plt.close()
+
+
+
+        ##### latency
+        retry_levels = run.keys()
+        print(run.keys())
+        points_in_time = list(run["1"].keys())
+        points_in_time_labels = [
+            "All Nodes Live", "1 Mix node Down", "2 Mix nodes Down", "3 Mix nodes Down"
+        ]
+
+        reliability_data = {
+            retry: [
+                convert_to_milliseconds(run[retry][str(pt)]["loopix_end_to_end_latency_seconds"]) 
+                for pt in points_in_time
+            ]
+            for retry in retry_levels
+        }
+
+        plt.figure(figsize=(20, 6))
+
+        bar_width = 0.2
+        group_spacing = 0.1
+        x_indexes = np.arange(len(points_in_time_labels)) * (len(retry_levels) * bar_width + group_spacing)
+
+        offset = 0
+
+        for retry, reliabilities in reliability_data.items():
+            reliability = [r for r in reliabilities]
+            plt.bar(
+                [x + offset for x in x_indexes],
+                reliability,
+                width=bar_width,
+                label=f"{retry} {variable}",
+            )
+            offset += bar_width
+
+        plt.title(f"Latency with {variable}", fontsize=16)
+        plt.ylabel("End-to-End Latency (ms)", fontsize=14)
+        plt.xticks([x + bar_width * (len(retry_levels) - 1) / 2 for x in x_indexes], points_in_time_labels, fontsize=12)
+        plt.legend(fontsize=12)
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+        save_plot_directory(directory)
+        plt.savefig(os.path.join(directory, f"{variable}_latency_churn_bar_chart.png"))
+        plt.clf()
+        plt.close()
+
+
+        ##### bandwidth
+        retry_levels = run.keys()
+        print(run.keys())
+        points_in_time = list(run["1"].keys())
+        points_in_time_labels = [
+            "All Nodes Live", "1 Mix node Down", "2 Mix nodes Down", "3 Mix nodes Down"
+        ]
+
+        reliability_data = {
+            retry: [
+                run[retry][str(pt)]["loopix_total_bandwidth_mb"] 
+                for pt in points_in_time
+            ]
+            for retry in retry_levels
+        }
+
+        plt.figure(figsize=(20, 6))
+
+        bar_width = 0.2
+        group_spacing = 0.1
+        x_indexes = np.arange(len(points_in_time_labels)) * (len(retry_levels) * bar_width + group_spacing)
+
+        offset = 0
+
+        for retry, reliabilities in reliability_data.items():
+            reliability = [r for r in reliabilities]
+            plt.bar(
+                [x + offset for x in x_indexes],
+                reliability,
+                width=bar_width,
+                label=f"{retry} {variable}",
+            )
+            offset += bar_width
+
+        plt.title(f"Total Network Usage over 300 seconds with {variable}", fontsize=16)
+        plt.ylabel("Network Usage (MB)", fontsize=14)
+        plt.xticks([x + bar_width * (len(retry_levels) - 1) / 2 for x in x_indexes], points_in_time_labels, fontsize=12)
+        plt.legend(fontsize=12)
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+        save_plot_directory(directory)
+        plt.savefig(os.path.join(directory, f"{variable}_bandwidth_churn_bar_chart.png"))
+        plt.clf()
+        plt.close()
+
 
 
 
